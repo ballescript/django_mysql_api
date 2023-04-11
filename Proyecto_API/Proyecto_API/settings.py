ab7c4e8a6f720d930c32e08fe726d7a56f4fe6d1
ballescript <ballescript@gmail.com>
2023-04-11 12:10:21 -0700

    secret key, decouple

--- Proyecto_API/Proyecto_API/settings.py
+++ Proyecto_API/Proyecto_API/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4gr70dw73w^d5jy0lyu2-+d3&!#u6uh5*%7#39n7qz^pabz86d'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
