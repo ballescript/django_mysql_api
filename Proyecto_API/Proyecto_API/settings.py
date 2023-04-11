d6f11fefe0c88e47d2502eb7b6ee95b937591f65
ballescript <ballescript@gmail.com>
2023-04-11 12:25:26 -0700

    env

--- Proyecto_API/Proyecto_API/settings.py
+++ Proyecto_API/Proyecto_API/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'localhost',
         'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': config("USER"),
+        'PASSWORD': config("PASSWORD"),
         'NAME': 'django_api',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
